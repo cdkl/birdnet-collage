20a33a13a42eeb6bb91b3fc1a43c0045852bd798
Chris Klein <seadecline@gmail.com>
2026-06-25 21:44:43 -0400

    deepseek can't indent

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ class Config:
     BIRDNET_GO_URL = os.getenv("BIRDNET_GO_URL", "http://birdnet-go.local:8080")
     BIRDNET_GO_URL = BIRDNET_GO_URL.rstrip("/")
     BIRDNET_GO_TOKEN = os.getenv("BIRDNET_GO_TOKEN", "")
-SITE_TITLE = os.getenv("SITE_TITLE", "birdnet collage")
+    SITE_TITLE = os.getenv("SITE_TITLE", "birdnet collage")
     HOST = os.getenv("HOST", "0.0.0.0")
     PORT = int(os.getenv("PORT", "8081"))
     DEBUG = os.getenv("DEBUG", "false").lower() == "true"
